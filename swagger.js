5b5290acfd51939a62efc57901077a8431083a9f
Henoc N'GASAMA <ngasamah@gmail.com>
2025-07-28 12:29:12 +0000

    Install swagger for documentation

---
+++ swagger.js
@@ -0,0 +1,26 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const env = require('dotenv');
+
+env.config();
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'Documentation de mon API Node.js avec Swagger'
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT}/api`
+      }
+    ]
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = { swaggerUi, specs };
