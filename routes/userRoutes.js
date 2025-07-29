57370d3eb798548ab438699327425e6ea52c8760
Henoc N'GASAMA <ngasamah@gmail.com>
2025-07-29 12:56:57 +0000

    Integration of login rate limit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const { register, login, refreshToken, getAllUsers, getUserById, deleteUser, updateProfile, changePassword, getUserOrders, getUserPrescriptions } = require('../controllers/userController');
 const auth = require('../middleware/auth');
+const rateLimit = require('express-rate-limit');
+
+
+const loginLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+  message: "Too many trying in 10 min. Please try again later.",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 
 router.get('/', auth, getAllUsers);
 router.get('/:id', auth, getUserById);
@@ -10,7 +21,7 @@ router.get('/:id/orders', auth, getUserOrders);
 router.get('/:id/prescriptions', auth, getUserPrescriptions);
 router.patch('/:id/change-password', auth, changePassword);
 router.post('/register', register);
-router.post('/login', login);
+router.post('/login', loginLimiter, login);
 router.post('/refresh-token', refreshToken);
 
 module.exports = router;
