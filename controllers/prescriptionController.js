bf5c1bdbc1e759da53f64afa915ffac102e45b56
Henoc N'GASAMA <ngasamah@gmail.com>
2025-07-29 10:49:49 +0000

    update of prescription

--- controllers/prescriptionController.js
+++ controllers/prescriptionController.js
@@ -49,6 +49,24 @@ exports.create = async (req, res) => {
 
 
 
+exports.update = async (req, res) => {
+  try {
+    const prescription = await Prescription.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body, doctor: req.user.id },
+      { new: true, runValidators: true }
+    ).populate('doctor', 'email firstName lastName');
+    
+    if (!prescription) return res.status(404).json({ error: 'Prescription not found' });
+    res.json(prescription);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+
+
+
 exports.delete = async (req, res) => {
   try {
     const prescription = await Prescription.findByIdAndDelete(req.params.id);
--- routes/prescriptionRoutes.js
+++ routes/prescriptionRoutes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { getAll, getOne, create, delete: deletePrescription } = require('../controllers/prescriptionController');
+const { getAll, getOne, create, update: updatePrescription, delete: deletePrescription } = require('../controllers/prescriptionController');
 const auth = require('../middleware/auth');
 const checkRole = require('../middleware/checkRole');
 
 router.get('/', auth, getAll);
 router.post('/', auth, checkRole('doctor'), create);
 router.get('/:id', auth, getOne);
+router.patch('/:id', auth, checkRole('doctor'), updatePrescription);
 router.delete('/:id', auth, checkRole('doctor'), deletePrescription);
 
 module.exports = router;
