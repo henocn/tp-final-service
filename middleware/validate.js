d34b288bdd667a4a0d4fcc9cc8abed9f6282a77d
Henoc N'GASAMA <ngasamah@gmail.com>
2025-08-04 19:39:08 +0000

    replace path by field in validation errors json

--- middleware/validate.js
+++ middleware/validate.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const formattedErrors = errors.array().map(err => ({
-      path: err.path,
+      field: err.path,
       msg: err.msg,
     }));
     return res.status(422).json({ errors: formattedErrors });
