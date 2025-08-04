eaef11bc64cbc190ef272e6b47ef0a35f9dcb04b
Henoc N'GASAMA <ngasamah@gmail.com>
2025-08-04 19:40:37 +0000

    Change routes medicines to integrate validators

--- routes/medicineRoutes.js
+++ routes/medicineRoutes.js
@@ -2,13 +2,15 @@
 const router = require('express').Router();
 const { getAll, getOne, create, bulkCreate, update, patch: patchMedicine, delete: deleteMedicine } = require('../controllers/medicineController');
 const auth = require('../middleware/auth');
-const { medicineValidator } = require('../validators/medicineValidator');
+const { createMedicineValidator, bulkCreateMedicineValidator, updateMedicineValidator } = require('../validators/medicineValidator');
+const validate = require('../middleware/validate');
+
 
 router.get('/', getAll);
 router.get('/:id', getOne);
-router.post('/', auth, create);
-router.post('/bulk', auth, bulkCreate);
-router.put('/:id', auth, update);
+router.post('/', auth, createMedicineValidator, validate, create);
+router.post('/bulk', auth, bulkCreateMedicineValidator, validate, bulkCreate);
+router.put('/:id', auth, updateMedicineValidator, validate, update);
 router.patch('/:id', auth, patchMedicine);
 router.delete('/:id', auth, deleteMedicine);
 
